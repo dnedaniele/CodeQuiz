40d9ad9bad4a337b397bd7903e83bd09128f02e7
Daniele Esposito <esp.dne@gmail.com>
2019-10-12 08:59:20 +0200

    timer done

--- questions.js
+++ questions.js
@@ -1,27 +1,49 @@
 
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    
-  ];
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
 
-  var startButton = document.querySelector("#start");
-  var HighScore = document.querySelector("#scoreNumb");
-  var CountDown = document.querySelector("#countdown");
+];
 
-  startButton.addEventListener('click', function () {
-console.log("click works")
-//                make visible the first question (no hide) 
-//                and start the count down
-  });
+var startButton = document.querySelector("#start");
+var HighScore = document.querySelector("#scoreNumb");
+var CountDown = document.querySelector("#countdown");
+
+HighScore = 0;
+
+
+startButton.addEventListener('click', function () {
+  console.log("click works")
+  setTime();
+  //start showing questions
+  var secondsLeft = 10;  // increase the number after testing
+
+
+  function setTime() {
+    var timerInterval = setInterval(timerDown, 1000);
+    function timerDown() {
+      secondsLeft--;
+      CountDown.textContent = secondsLeft;
+
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        console.log('time out');
+        //sendMessage(Score, show timeOut);
+      }
+    }
+  }
+
+  //                make visible the first question (no hide) 
+  //                and start the count down
+});
 
   //***************  Pseudo Code **************** //
 // 1. botton "start the Quiz" (event 1)
