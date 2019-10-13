68e8b57e5475b3f323043bf1cd91b71d5fc69638
Daniele Esposito <esp.dne@gmail.com>
2019-10-13 20:35:16 +0200

    questions creations and object works

--- questions.js
+++ questions.js
@@ -13,10 +13,27 @@ var questions = [
 
 ];
 
+//***************  Pseudo Code **************** //
+// 1. botton "start the Quiz" (event 1)
+//                make visible the first question (no hide) 
+//                and start the count down
+// // 2. when I click on the answer (event2)
+//                if the answer is right - 
+                //          correct message 
+                //          a. add point - 
+                // else 
+                //          wrong message 
+                //          no point++ - 
+                //          subtract time
+//                after 2 secs the second question appears
+// 3. time runs out and/or all questions are answered: 
+        //        show final score.
+
+
 var startButton = document.querySelector("#start");
 var HighScore = document.querySelector("#scoreNumb");
 var CountDown = document.querySelector("#countdown");
-
+//var numQuest = for (k = 0; )
 HighScore = 0;
 
 var question1 = document.querySelector('#q1');
@@ -24,10 +41,11 @@ var question1 = document.querySelector('#q1');
 var question2 = document.querySelector('#q2');
     question2.style.display = 'none';
 
+    //Start the game
 startButton.addEventListener('click', function () {
   console.log("click works")
   setTime();
-  showQuestion();
+  Show();
   var secondsLeft = 10;  // increase the number after testing
 
 
@@ -44,38 +62,58 @@ startButton.addEventListener('click', function () {
       }
     }
   }
-  function showQuestion() {
+
+  function Show() {
     var question1 = document.querySelector('#q1');
     question1.style.display = 'block';
-  };
-  function hideQuestion () {
+  }
+  function hide () {
     var question1 = document.querySelector('#q1');
     question1.style.display = 'none';
   }
-    //if (question1.style.display == "none") {
-    //   question1.style.display = "none" ;
-    // } else {
-    //   question1.style.display = "block";
-    // }
-   
 
-  //                make visible the first question (no hide) 
-  //                and start the count down
-});
 
-  //***************  Pseudo Code **************** //
-// 1. botton "start the Quiz" (event 1)
-//                make visible the first question (no hide) 
-//                and start the count down
-// // 2. when I click on the answer (event2)
-//                if the answer is right - 
-                //          correct message 
-                //          a. add point - 
-                // else 
-                //          wrong message 
-                //          no point++ - 
-                //          subtract time
-//                after 2 secs the second question appears
-// 3. time runs out and/or all questions are answered: 
-        //        show final score.
+})
+  ///FUNCTIONS
+
+// Hide and Show
+  function Show() {
+    var question1 = document.querySelector('#q1');
+    question1.style.display = 'block';
+  }
+  //   for (i = 0; i < numQuest.length; i++){
+
+  //   }
+    
+  function hide () {
+    var question1 = document.querySelector('#q1');
+    question1.style.display = 'none';
+  }
+
+    
+   //console.log(questions[0].title);
+
+// for (var i = 0; i < questions.length; i++) {
+// if (questions[i] === questions.title);           //  APPROVED
+// console.log("build question");// buildQuestion ()
+//  }
+
+ for (var i = 0; i < questions.length; i++) {             // APRROVED
+  //if (questions[i] === questions.title);           
+   //console.log(questions[i].title + questions[i].choices );// buildQuestion ()
+   //console.log(questions[i].choices[i]);
+  //var newEl = document.createElement('div');
+  for (var i = 0; i < questions[i].lenght; i++) {
+  console.log("hi");
+  //console.log(questions[i].choices);
+  // // console.log(questions[i].choices);
+  //   // var btn = document.createElement("BUTTON");
+  //   // btn.innerHTML = questions[i].choices[i];
+  //   // document.body.appendChild(btn);
+     }
+  }
+
+  
+
+ 
 
