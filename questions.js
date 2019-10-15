27b98c4b4be7443b7f4a27b774bf509028c3089a
Daniele Esposito <esp.dne@gmail.com>
2019-10-15 09:35:15 +0200

    print questions

--- questions.js
+++ questions.js
@@ -36,19 +36,19 @@ var CountDown = document.querySelector("#countdown");
 //var numQuest = for (k = 0; )
 HighScore = 0;
 
+
 var question1 = document.querySelector('#q1');
     question1.style.display = 'none';
 var question2 = document.querySelector('#q2');
     question2.style.display = 'none';
 
+
     //Start the game
 startButton.addEventListener('click', function () {
-  console.log("click works")
-  setTime();
-  Show();
+
+  // TIME
   var secondsLeft = 10;  // increase the number after testing
-
-
+  setTime();
   function setTime() {
     var timerInterval = setInterval(timerDown, 1000);
     function timerDown() {
@@ -62,56 +62,50 @@ startButton.addEventListener('click', function () {
       }
     }
   }
-
+  // QUESTIONS
+  Show();
   function Show() {
     var question1 = document.querySelector('#q1');
     question1.style.display = 'block';
   }
-  function hide () {
-    var question1 = document.querySelector('#q1');
-    question1.style.display = 'none';
-  }
-
+  
 
 })
-  ///FUNCTIONS
+  
+///FUNCTIONS
 
 // Hide and Show
-  function Show() {
-    var question1 = document.querySelector('#q1');
-    question1.style.display = 'block';
-  }
-  //   for (i = 0; i < numQuest.length; i++){
+function Show() {
+  var question1 = document.querySelector('#q1');
+  question1.style.display = 'block';
+}
+//   for (i = 0; i < numQuest.length; i++){
 
-  //   }
+//   }
+  
+function hide () {
+  var question1 = document.querySelector('#q1');
+  question1.style.display = 'none';
+}
+
+//failed experiments
+
+// function ShowQest1 () {
+//   console.log(questions[0].title)
+//   var text = document.createElement('div') //here u write new html
+//   text.textContent = questions[0].title;   // I write the content for the html
+//   document.body.appendChild(text);   /// I put the content inside 
+
+// for (var i = 0; i < questions[0].choices.length; i++) {
+//   var btn = document.createElement("BUTTON");
+//    btn.innerHTML = questions[0].choices[i];
+//     document.body.appendChild(btn);
+// }
+// }
+
+
     
-  function hide () {
-    var question1 = document.querySelector('#q1');
-    question1.style.display = 'none';
-  }
-
-    
-   //console.log(questions[0].title);
-
-// for (var i = 0; i < questions.length; i++) {
-// if (questions[i] === questions.title);           //  APPROVED
-// console.log("build question");// buildQuestion ()
-//  }
-
- for (var i = 0; i < questions.length; i++) {             // APRROVED
-  //if (questions[i] === questions.title);           
-   //console.log(questions[i].title + questions[i].choices );// buildQuestion ()
-   //console.log(questions[i].choices[i]);
-  //var newEl = document.createElement('div');
-  for (var i = 0; i < questions[i].lenght; i++) {
-  console.log("hi");
-  //console.log(questions[i].choices);
-  // // console.log(questions[i].choices);
-  //   // var btn = document.createElement("BUTTON");
-  //   // btn.innerHTML = questions[i].choices[i];
-  //   // document.body.appendChild(btn);
-     }
-  }
+   
 
   
 
